938e6e9e6bb059168ceb1d74e4e23e0c9d91fb60
Marcos <jr.zaghetto@gmail.com>
2020-11-09 10:15:12 -0300

    Limpeza no código Nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,95 +1,82 @@
-<style>
-	* {box-sizing: border-box;}
-
-.topnav {
-  overflow: hidden;
-  background-color: #fff;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-}
-
-.topnav img {
-  justify-items: start;
-  padding-left: 10px;
-  padding-top: 6px;
-}
-
-.topnav .search-container {
-  float: right;
-  background-color: #abcdfa;
-}
-
-.topnav input[type=text] {
-  padding: 6px;
-  margin-top: 8px;
-  font-size: 17px;
-  border: none;
-  background: transparent;
-  color: white;
-  float: left;
-  margin-left: 10px;
-  letter-spacing: 0.3px;
-    font-weight: 300;
-  width: 85%;
-}
-
-.topnav .search-container button {
-  float: left;
-  padding: 13px 13px;
-  margin-top: 10px;
-  margin-left: 20px;
-  background: transparent;
-  font-size: 17px;
-  border: none;
-  cursor: pointer;
-  filter: invert(1);
-  background-image: url('/images/search.svg');
-}
-
-::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-  color: white;
-  opacity: 1; /* Firefox */
-}
-
-:focus{outline: none;}
-
-@media screen and (max-width: 600px) {
-  .topnav .search-container {
-    float: none;
-  }
-  
-  .topnav input[type=text], .topnav .search-container button {
-    float: none;
-    display: block;
-    text-align: left;
-    width: 100%;
-    margin: 0;
-    padding: 14px;
-  }
-  .topnav input[type=text] {
-    border: 1px solid #ccc;  
-  }
-
-}
-</style>
-
 <script>
-  let src = 'images/logo.svg'
+  let src = "images/logo.svg";
 
   function alerta() {
-		alert('Não implementei a pesquisa :(')
-		}
-
+    alert("Não implementei a pesquisa :(");
+  }
 </script>
 
+<style>
+  * {
+    box-sizing: border-box;
+  }
 
+  .topnav {
+    overflow: hidden;
+    background-color: #fff;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  .topnav img {
+    justify-items: start;
+    padding-left: 10px;
+    padding-top: 6px;
+  }
+
+  .topnav .search-container {
+    float: right;
+    background-color: #abcdfa;
+  }
+
+  .topnav input[type="text"] {
+    padding: 6px;
+    margin-top: 8px;
+    font-size: 17px;
+    border: none;
+    background: transparent;
+    color: white;
+    float: left;
+    margin-left: 10px;
+    letter-spacing: 0.3px;
+    font-weight: 300;
+    width: 85%;
+  }
+
+  .topnav .search-container button {
+    float: left;
+    padding: 13px 13px;
+    margin-top: 10px;
+    margin-left: 20px;
+    background: transparent;
+    font-size: 17px;
+    border: none;
+    cursor: pointer;
+    filter: invert(1);
+    background-image: url("/images/search.svg");
+  }
+
+  ::placeholder {
+    /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: white;
+    opacity: 1; /* Firefox */
+  }
+
+  :focus {
+    outline: none;
+  }
+</style>
 
 <div class="topnav">
-  <img {src} alt="asd" width="80px">
+  <img {src} alt="asd" width="80px" />
   <div class="search-container">
-    <form on:click={alerta}>
-      <button type="submit"><i class="fa fa-search"></i></button>
-      <input class="effect-1" type="text" placeholder="Pesquise um PokeMão..." name="search">
+    <form on:click={alerta} on:submit|preventDefault>
+      <button type="submit" />
+      <input
+        class="effect-1"
+        type="text"
+        placeholder="Pesquise um PokeMão..."
+        name="search" />
     </form>
   </div>
 </div>
